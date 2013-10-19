ac3aa21d1c09064234a322addbbd45d2f582e162
Richard Downer <richarddowna@gmail.com>
2013-10-19 14:09:47 +1300

    always transform

--- hello.exs
+++ hello.exs
@@ -11,3 +11,11 @@ list 1 = [ 3, 2, 1 ]
 # [ 3, 2, 1 ]
 list 2 = [ 4 | list1 ]
 # [ 4, 3, 2, 1 ]
+
+# we always transform data, we never modify it in place
+name = "elixir"
+# "elixir"
+cap_name = String.capitalize name
+# "Elixir"
+name
+# "elixir"
